5919e6c1ead3e8e82499ae100a4d2d11c5bbe5e9
tkawabat <tosiki.kawabata@gmail.com>
2022-11-18 04:53:11 +0900

    add .ssh/config

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+function make_link() {
+    if [ -e $1 ]; then
+        echo "$1 already exists. skip create."
+    else
+        echo "create $1 link."
+        ln -s $2 $1
+    fi
+}
+
 for F in .??*
 do
     if [ "$F" = ".git" ]; then continue; fi
     if [ "$F" = ".gitignore" ]; then continue; fi
     if [ "$F" = ".DS_Store" ]; then continue; fi
 
-    if [ ! -e ~/$F ]; then 
-        echo "create $F"
-        ln -s `pwd`/$F ~/$F
-    fi
+    make_link ~/$F `pwd`/$F
 done
+
+make_link ~/.ssh/config `pwd`/.ssh/config
 
 if [ ! -e ~/local ]; then mkdir ~/local; fi
 if [ ! -e ~/pkg ]; then mkdir ~/pkg; fi
